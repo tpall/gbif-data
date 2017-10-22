2bba6a3fb637bd5f8660af02ff1551c7fb0fadd1
Taavi Päll <tapa741@gmail.com>
2017-10-22 22:09:27 +0300

    top100 spec

--- R/import.R
+++ R/import.R
@@ -2,3 +2,11 @@ library(tidyverse)
 gbif <- read_delim("data/0000690-171020152545675.csv", "\t", 
                    escape_double = FALSE, trim_ws = TRUE)
 
+top100_species <- gbif$specieskey %>% 
+  table() %>% 
+  as_data_frame() %>% 
+  arrange(desc(n)) %>% 
+  top_n(100, n)
+
+
+
